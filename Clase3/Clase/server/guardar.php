ee846f969afdc2e931adac31cb97959af2607fb4
alumno <alumno>
2017-04-03 22:03:18 -0300

    Ejercicio Formulario

--- Clase3/Clase/server/guardar.php
+++ Clase3/Clase/server/guardar.php
@@ -1,8 +1,17 @@
 <?php
     var_dump($_REQUEST);//
     var_dump($_POST);// trae lo que trae por un post
-    $nombre = $_POST['nombre'];//accedo al input que trae el nombre
-    $archivo = fopen("datos.txt","w");
-    fwrite($archivo,$nombre);
-    fclose($archivo);
+    if(in_array($_POST,"escribir") && $_POST['escribir'] == "escribir")
+    {
+        $nombre = $_POST['nombre'];//accedo al input que trae el nombre
+        $archivo = fopen($_POST['archivo'],"w");
+        fwrite($archivo,$nombre);
+        fclose($archivo);
+    }
+    elseif(in_array($_POST,"leer") && $_POST['leer'] == "leer")
+    {
+        $archivo = fopen("datos.txt","r");
+        echo fgets($archivo);
+        fclose($archivo);
+    }  
 ?>
