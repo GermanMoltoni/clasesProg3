59fe501e4cca4a6a460dd7f535067fa71700095b
German Moltoni <german.moltoni@gmail.com>
2017-04-08 01:43:52 -0300

    Ejercicio 22

--- Clase3/Ejercicios/Ejercicio22/copia.php
+++ Clase3/Ejercicios/Ejercicio22/copia.php
@@ -1,4 +1,7 @@
 <?php
-if(array_key_exists('archivo',$_POST))
+if(array_key_exists('archivo',$_POST) && file_exists($_POST['archivo']))
+{
     copy($_POST['archivo'],"./misArchivos/".date("Y_m_d_h_i_s").".txt");
+}
+
 ?>
