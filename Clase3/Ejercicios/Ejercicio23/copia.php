679f47f76f17366055447703c921d5d1801229db
German Moltoni <german.moltoni@gmail.com>
2017-04-09 13:26:06 -0300

    Ejercicio 23

--- Clase3/Ejercicios/Ejercicio23/copia.php
+++ Clase3/Ejercicios/Ejercicio23/copia.php
@@ -3,7 +3,12 @@ if(array_key_exists('archivo',$_POST) && file_exists($_POST['archivo']))
 {
     $archivo = fopen($_POST['archivo'],"r");
     $archivoNuevo = fopen("./misArchivos/".date("Y_m_d_h_i_s").".txt","w");
-    fwrite($archivoNuevo,strrev($archivo));
+    while(!feof($archivo))
+    {
+        $string = fgets($archivo);
+        fwrite($archivoNuevo,strrev($string));
+    }
+    
     fclose($archivo);
     fclose($archivoNuevo);
 }
