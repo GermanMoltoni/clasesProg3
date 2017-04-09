b6a25da14c522e03c76fd7b676786e8a2228bfcd
German Moltoni <german.moltoni@gmail.com>
2017-04-09 13:15:42 -0300

    Ejercicio 24

--- Clase3/Ejercicios/Ejercicio24/enigma.php
+++ Clase3/Ejercicios/Ejercicio24/enigma.php
@@ -14,15 +14,27 @@ class Enigma
         if(strlen($mensaje) != 0)
         {
             foreach(str_split($mensaje) as $caracter)
-            {
                 $cifrado.=ord($caracter) + 200;
-            }
             // $archivo = fopen($path,"w");
             // fwrite($archivo,$cifrado);
             // fclose($archivo);
         }
     }
     static function DesEncriptar($path)
+    {
+        // if(file_exists($path))
+        // {
+            // $archivo = fopen($path,"r");
+            // while(!feof($archivo))
+            // {
+                // $mensaje = fgets($archivo);
+                
+                // fclose($archivo);
+            // }
+            
+
+        // }
+    }
 }
 
 
