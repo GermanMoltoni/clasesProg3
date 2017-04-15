3250e6529034311934a0f6ac19bbbb02bbfb4568
German Moltoni <german.moltoni@gmail.com>
2017-04-15 01:02:22 -0300

    Empleados Nuevos

--- Tp/php/mostrar.php
+++ Tp/php/mostrar.php
@@ -2,9 +2,12 @@
     require_once "empleado.php";
     $path = "../datos/empleados.txt";
     $empleados = array();
+    $tabla ='';
     if(file_exists($path))
     {
         $archivo = fopen($path,"r");
+        $tabla .= "<table class='table-bordered table-striped'>";
+        $tabla.="<tr><th>Nombre</th><th>Apellido</th><th>Sexo</th><th>Dni</th><th>Legajo</th><th>Foto</th></tr>";
         while(!feof($archivo))
         {
             $datos = explode("-",fgets($archivo));
@@ -12,11 +15,12 @@
             {
                 $empleado = new empleado($datos[0],$datos[1],$datos[3],$datos[2],$datos[5],$datos[4]);
                 $empleado->setPathFoto($datos[6]."-".$datos[7]);
-                array_push($empleados,$empleado);  
-                echo $empleado->ToString()."<img src='".$empleado->getPathFoto()."' alt='200' width='299'><br>";
+                array_push($empleados,$empleado);
+                $tabla.="<tr><td>".$empleado->getNombre()."</td><td>".$empleado->getApellido()."</td><td>".$empleado->getSexo()."</td><td>".$empleado->getDni()."</td><td>".$empleado->getLegajo()."</td><td>"."<img src='".$empleado->getPathFoto()."' alt='' width='50'></td></tr>";
             }
-       
         }
+        $tabla.="</table>";
+        echo $tabla;
         fclose($archivo);
         
     }
