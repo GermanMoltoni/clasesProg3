0d8f47ea1fafc82b30c3018345cc96abae145bb1
alumno <alumno>
2017-04-17 21:51:00 -0300

    Db

---
+++ Clase5/DB/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+$conexion = mysqli_connect("localhost","root","","ejemploUno");
+$id = 2;
+$textoConsulta = "SELECT * FROM usuario WHERE id=".$id;
+$consulta =  mysqli_query($conexion,$textoConsulta);
+//var_dump($consulta);
+$filas = mysqli_fetch_object($consulta);
+var_dump($filas);
+mysqli_close($conexion);*/
+?>
