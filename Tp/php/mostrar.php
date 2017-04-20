f89b536d2d1f02f9e5f167a339d70f32953988a3
GermanMoltoni <german.moltoni@gmail.com>
2017-04-20 01:45:59 -0300

    Mostrar con boton en tabla

--- Tp/php/mostrar.php
+++ Tp/php/mostrar.php
@@ -8,7 +8,7 @@
     {
         $archivo = fopen($path,"r");
         $tabla .= "<table class='table bg-info table-striped table-condensed'>";
-        $tabla.="<thead  class='bg-primary'><tr><th class='col-md-2 '>Nombre</th><th class='col-md-2'>Apellido</th><th class='col-md-2'>Sexo</th><th class='col-md-2' >Dni</th><th class='col-md-2'>Legajo</th><th class='col-md-2'>Foto</th></tr></thead>";
+        $tabla.="<thead  class='bg-primary'><tr><th class='col-md-2 '>Nombre</th><th class='col-md-2'>Apellido</th><th class='col-md-2'>Sexo</th><th class='col-md-2' >Dni</th><th class='col-md-2'>Legajo</th><th class='col-md-1'>Foto</th><th class='col-md-1'>Baja</th></tr></thead>";
         while(!feof($archivo))
         {
             $datos = explode("-",fgets($archivo),7);
@@ -17,7 +17,7 @@
                 $empleado = new empleado($datos[0],$datos[1],$datos[3],$datos[2],$datos[5],$datos[4]);
                 $empleado->setPathFoto($datos[6]);
                 array_push($empleados,$empleado);
-                $tabla.="<tr><td>".$empleado->getNombre()."</td><td>".$empleado->getApellido()."</td><td>".$empleado->getSexo()."</td><td>".$empleado->getDni()."</td><td>".$empleado->getLegajo()."</td><td>"."<img src='".$empleado->getPathFoto()."' class=''alt='' width='50'></td></tr>";
+                $tabla.="<tr><td>".$empleado->getNombre()."</td><td>".$empleado->getApellido()."</td><td>".$empleado->getSexo()."</td><td>".$empleado->getDni()."</td><td>".$empleado->getLegajo()."</td><td>"."<img src='".$empleado->getPathFoto()."' class=''alt='' width='50'></td><td><button name='baja' onClick='baja(".$empleado->getLegajo().")'class='btn btn-danger navbar-btn'>Baja</button></td></tr>";
             }
         }
         $tabla.="</table>";
