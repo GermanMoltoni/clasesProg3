53168764e3d8d4dc16fe65c0359826fc45249179
German Moltoni <german.moltoni@gmail.com>
2017-05-07 20:21:00 -0300

    Clases

---
+++ Clases_php/archivos.php
@@ -0,0 +1,48 @@
+<?php
+
+class Archivo{
+    private $_path;
+    function __construct($path)
+    {
+        $this->_path = $path;
+    }
+
+    function ToFile($datos,$mode="a")
+    {
+        $file = fopen($this->_path,$mode);
+        fwrite($file,$datos);
+        fclose($file);
+    }
+    function GetArray(){
+        if(file_exists($this->_path))
+        {
+            $file = fopen($this->_path,"r");
+            $array = array();
+            while(!feof($file))
+            {
+                $line = explode("-",fgets($file),3);
+                array_push($array,$line);//Crear Objetos
+            }
+            fclose($file);
+            return $array;
+        }
+        return false;
+    }
+
+    function ToJSON($datos){
+        $this->ToFile(json_encode($datos));
+    }
+
+    function GetJson(){
+        if(file_exists($this->_path))
+        {
+            $file = fopen($this->_path,"r");
+            while(!feof($file))
+                $datos = fgets($file);
+            fclose($file);
+            return json_decode($datos);
+        }
+        return false;
+    }
+}
+?>
