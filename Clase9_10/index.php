0ef60d416828f9880a9a4e2b7fc9eef7d59c8046
alumno <alumno>
2017-06-05 21:42:59 -0300

    con formdata

--- Clase9_10/index.php
+++ Clase9_10/index.php
@@ -5,6 +5,14 @@ require './cd.php';
 require './vendor/autoload.php';
 require './AccesoDatos.php';
 $app = new \Slim\App;
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
