164cc6e0eda5a05c95332194848679915e2eaffb
German Moltoni <german.moltoni@gmail.com>
2017-06-28 22:41:06 -0300

    funcionando

--- slim_middleware/index.php
+++ slim_middleware/index.php
@@ -5,15 +5,21 @@ use Psr7Middlewares\Middleware;
 use Psr7Middlewares\Middleware\ClientIp;
 use Psr7Middlewares\Middleware\DetectDevice;
 use Psr7Middlewares\Middleware\Geolocate;
+
 require './vendor/autoload.php';
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 $app = new \Slim\App(["settings" => $config]);
 $app->get('/', function (Request $request, Response $response) {
- var_dump(Geolocate::getLocation($request));
-    return $response->withJson(array('ip'=>ClientIp::getIp($request),'device'=>DetectDevice::getDevice($request)->isMobile()));
+ $data = Geolocate::getLocation($request)->first();
+
+    return $response->withRedirect('https://maps.google.com/?q='.$data->getLatitude().','.$data->getLongitude());
+    return $response->withJson(array('ip'=>ClientIp::getIp($request),'device'=>DetectDevice::getDevice($request)->isMobile(),'lugar'=>[]));
 
     
 })->add(Middleware::DetectDevice())->add(Middleware::Geolocate())->add(Middleware::ClientIp());
 $app->run();
+
+
+
